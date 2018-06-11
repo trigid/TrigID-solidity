db07d6c11e1e95fb29fed969934c165e9af8421a
keithomiping <keith.omiping@factnexus.com>
2018-06-11 13:27:26 +0800

    Added package.json to install NPM dependencies

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,6 +4,3 @@ curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
 sudo yum install nodejs
 sudo npm install -g truffle
 sudo npm install -g ganache-cli
-# required for SafeMath test
-sudo npm install --save-dev chai
-sudo npm install --save-dev chai-bignumber
