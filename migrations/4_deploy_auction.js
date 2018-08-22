16697d31051efbf5be50fe9b0723e9bfa2f87ffd
keithomiping <keith.omiping@factnexus.com>
2018-08-22 14:56:06 +0800

    Removed auction

--- migrations/4_deploy_auction.js
+++
@@ -1,7 +0,0 @@
-var Auction = artifacts.require("./Auction.sol");
-
-module.exports = function(deployer, network, accounts) {
-  const wallet = accounts[1];
-  const priceFactor = 10.55;
-  deployer.deploy(Auction, wallet, priceFactor);
-};
